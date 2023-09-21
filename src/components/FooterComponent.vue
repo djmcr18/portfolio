0a2752a3e1faa8b26a85e1e26938ae4f712c4f91
Diego Miranda <diego.mirandacr.18@gmail.com>
2023-09-21 23:20:09 +1000

    1.3P Final Commit 3

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -8,9 +8,9 @@
         <div class="footer-section">
           <h4>Navigation</h4>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Blog</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
             <li><a href="https://github.com/djmcr18/space_game" target="_blank">My Game Project</a></li>
             <li><a href="https://github.com/djmcr18" target="_blank">My GitHub</a></li>
           </ul>
